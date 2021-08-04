5423ecb02174c8e050b66f0fa094be00ba9d9047
Garazi <ggartzia@gmail.com>
2021-08-04 11:45:22 +0200

    Format config file

--- news_puller/config.py
+++ news_puller/config.py
@@ -1,13 +1,46 @@
 paper_list = [
-    "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada",
-    "https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/internacional/portada",
-    "https://www.abc.es/rss/feeds/abc_EspanaEspana.xml",
-    "https://www.abc.es/rss/feeds/abc_Internacional.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml",
-    "https://e00-elmundo.uecdn.es/elmundo/rss/internacional.xml",
-    "https://cronicaglobal.elespanol.com/es/rss/general-001.xml",
-    "https://rss.elconfidencial.com/espana/",
-    "https://rss.elconfidencial.com/mundo/",
-    "https://okdiario.com/feed",
-    "https://www.esdiario.com/rss/articulos.xml"
+    {
+      'paper': 'EL PAÍS', 
+      'feed': 'https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada'
+    },
+    {
+      'paper': 'EL PAÍS', 
+      'feed': 'https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/section/internacional/portada'
+    },
+    {
+      'paper': 'El Mundo', 
+      'feed': 'https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml'
+    },
+    {
+      'paper': 'El Mundo', 
+      'feed': 'https://e00-elmundo.uecdn.es/elmundo/rss/internacional.xml'
+    },
+    {
+      'paper': 'El Español', 
+      'feed': 'https://cronicaglobal.elespanol.com/es/rss/general-001.xml'
+    },
+    {
+      'paper': 'El Confidencial', 
+      'feed': 'https://rss.elconfidencial.com/espana/'
+    },
+    {
+      'paper': 'El Confidencial', 
+      'feed': 'https://rss.elconfidencial.com/mundo/'
+    },
+    {
+      'paper': 'OKDiario', 
+      'feed': 'https://okdiario.com/feed'
+    },
+    {
+      'paper': 'El Diario',
+      'feed': 'https://www.esdiario.com/rss/articulos.xml'
+    },
+    #{
+    #  'paper': 'ABC', 
+    #  'feed': 'https://www.abc.es/rss/feeds/abc_EspanaEspana.xml'
+    #},
+    #{
+    #  'paper': 'ABC', 
+    #  'feed': 'https://www.abc.es/rss/feeds/abc_Internacional.xml'
+    #}
 ]
