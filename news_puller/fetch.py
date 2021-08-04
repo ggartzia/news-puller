4fd26eefd3bdcdd22f33ceb5f84520308bc5e4c0
Garazi <ggartzia@gmail.com>
2021-08-04 11:44:48 +0200

    Fetch and filter fields from main newspapers

--- news_puller/fetch.py
+++ news_puller/fetch.py
@@ -1,20 +1,29 @@
-import news_puller.save
 import numpy as np
 import news_puller.config as cfg
 import feedparser
 import logging
 
 
-try:
-    for paper in cfg.paper_list:
-        logging.debug(paper)
-        d = feedparser.parse(paper)
+feed_fields = ['title', 'link', 'published', 'tags', 'media_thumbnail', 'media_content']
 
-except:
-    logging.warning('Failed to load USE model, USE API won\'t be available')
-
+def filter_feed(paper, news):
+  for idx, item in enumerate(news):
+    news[idx] = {k: v for k, v in item.items() if k in feed_fields}.update({'paper', paper})
+  
+  return news
 
 def get_news():
-    news = session.run()
+    news = []
+
+    try:
+      for plist in cfg.paper_list:
+        logging.debug('Fetch news from ' + plist['paper'])
+
+        paper_news = feedparser.parse(plist['feed'])
+        paper_news = filter_feed(plist['paper'], paper_news.entries)
+        news = news + paper_news
+
+    except:
+      logging.warning('Failed to load USE model, USE API won\'t be available')
 
     return np.array(news).tolist()
