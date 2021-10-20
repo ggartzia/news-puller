25443dfbc5415abf5f331bb7cddd0e8e4366ccc7
Garazi <ggartzia@gmail.com>
2021-10-20 14:27:21 +0200

    Include fetching debug

--- news_puller/fetch.py
+++ news_puller/fetch.py
@@ -1,12 +1,16 @@
 import numpy as np
 import news_puller.config as cfg
 import feedparser
-import logging
+from logging import getLogger, DEBUG
+
+log = getLogger('werkzeug')
+log.setLevel(DEBUG)
 
 feed_fields = ['title', 'link', 'published', 'tags', 'media_thumbnail', 'media_content']
 
 
 def filter_feed(paper, news):
+    log.debug(news)
     for idx, item in enumerate(news):
         news[idx] = {k: v for k, v in item.items() if k in feed_fields}.update({'paper', paper})
 
@@ -16,15 +20,19 @@ def filter_feed(paper, news):
 def get_news():
     news = []
 
-    try:
-        for plist in cfg.paper_list:
-            logging.debug('Fetch news from ' + plist['paper'])
+    for plist in cfg.paper_list:
+        log.debug('Fetch ' + plist['paper'] + ' news from ' + plist['feed'])
 
+        try:
             paper_news = feedparser.parse(plist['feed'])
-            paper_news = filter_feed(plist['paper'], paper_news.entries)
-            news = news + paper_news
+            log.debug(paper_news)
+            if paper_news.status == 200:
+                news = news + filter_feed(plist['paper'], paper_news['entries'])
 
-    except:
-        logging.warning('Failed to load USE model, USE API won\'t be available')
+            else:
+                log.warning("Some connection error", paper_news.status)
+
+        except:
+            log.error('Failed to load USE model, USE API won\'t be available')
 
     return np.array(news).tolist()
