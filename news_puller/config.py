a24a20fc0e469e18ef1e752cc1ec5fafd54c599e
Garazi <ggartzia@gmail.com>
2021-12-18 00:13:47 +0100

    Update config.py

--- news_puller/config.py
+++ news_puller/config.py
@@ -1,4 +1,4 @@
-paper_list = [
+PAPER_LIST = [
     {
       'paper': 'El Mundo', 
       'feed': 'https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml'
@@ -28,10 +28,6 @@ paper_list = [
       'feed': 'https://www.esdiario.com/rss/articulos.xml'
     },
     {
-      'paper': 'El Periodico',
-      'feed': 'https://www.elperiodico.com/es/rss/rss_portada.xml'
-    },
-    {
       'paper': 'Marca',
       'feed': 'https://e00-marca.uecdn.es/rss/portada.xml'
     },
@@ -40,3 +36,17 @@ paper_list = [
       'feed': 'https://as.com/rss/tags/ultimas_noticias.xml'
     }
 ]
+
+MONGO_USERNAME = 'ggartzia'
+MONGO_PASSWORD = '2cvmAjSFeyb4Pu4m'
+
+TW_CONSUMER_KEY = '2fg7N4KJRfAhHVqlPeGNDnl3M'
+TW_CONSUMER_SECRET = '6qpUaowQbnzaKJ91qSshhuixW0JqUasCVaG5n8g7Iju2JuHMy7'
+TW_ACCESS_TOKEN = '2176417916-HMOxb8mBYu0juMpCqMA4WXlNVXN1TYM221Q6sl6'
+TW_ACCESS_TOKEN_SECRET = 'xF6Dx57Bn3WQP2wtJeDGYLm9pBpCJSP5CDy5fXANeq7HB'
+TW_BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAABHDSAEAAAAAoPEstyMcrbCCZH51cLpb%2BsSVwUc%3D7Y8xzFFLygChgpa6QOA9d3e3uzhmuRBBt6FmZ03AxHJw1nEMXh'
+
+GOOGLE_API_URL = 'https://factchecktools.googleapis.com/v1alpha1/claims:search'
+GOOGLE_API_KEY = 'xxxxxx'
+
+TF_IDF_MIN_WEIGHT = 3
