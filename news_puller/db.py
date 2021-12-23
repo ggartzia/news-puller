321f39521136d97b2421cf65b8a77f745295e62b
Garazi <ggartzia@gmail.com>
2021-12-23 14:48:01 +0100

    Remove logs

--- news_puller/db.py
+++ news_puller/db.py
@@ -25,21 +25,17 @@ class Database(object):
         
         try:
             idfs = {}
-            print('Analize this title: ' + title)
+            
             title = title.lower()
             title = re.sub("[^A-Za-zÑñÁáÉéÍíÓóÚú]", " ", title)
-            print('Clean: ' + title)
+
             for term in title.split():
                 # Use the number of docs that contain the term to calculate the IDF
                 term_docs = Database.DATABASE['news'].count_documents({'title' : {'$regex' : term}})
-                print('Num: ' + str(term_docs))
-                print('Num2: ' + str(num_docs - term_docs + 0.5))
-                print('Num3: ' + str(term_docs + 0.5))
                 idfs[term] = log((num_docs - term_docs + 0.5) / (term_docs + 0.5))
 
             idfs = {k: v for k, v in idfs.items() if v > cfg.TF_IDF_MIN_WEIGHT}
             
-            print('Analize this title: ' + str(idfs))
             topics = list(idfs.keys())
         
         except Exception as e:
@@ -118,7 +114,6 @@ class Database(object):
         
         mongo_db = Database.DATABASE['news']
 
-        print('Try')
         new = mongo_db.find_one({'paper' : media, 'theme' : theme}, sort=[('published', pymongo.DESCENDING)])
-        print('The last new is: ' + str(new))
+
         return new['published']
