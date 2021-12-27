a15826f626b3fb1df449f3f57e79827f80580e24
Garazi <ggartzia@gmail.com>
2021-12-27 12:37:24 +0100

    Add twitter count fetch on scheduler

--- news_puller/scheduler.py
+++ news_puller/scheduler.py
@@ -9,9 +9,14 @@ log.setLevel(DEBUG)
 
 
 def news_update(theme):
-    # Fetch the last time it was executed and fetch only the news after that time
     print('news_update for ' + theme + ' media!')
     api_url = cfg.HOST_URL + 'fetch/' + theme
+    response = requests.get(api_url)
+
+
+def twitter_update():
+    print('Update the number of times a new was shared on twitter')
+    api_url = cfg.HOST_URL + 'update_twitter_counts/24'
     response = requests.get(api_url)
 
 
@@ -19,7 +24,7 @@ scheduler = BackgroundScheduler(timezone="Europe/Berlin")
 scheduler.add_job(lambda: news_update('noticias'), 'interval', hours=1)
 scheduler.add_job(lambda: news_update('deportes'), 'interval', hours=2)
 scheduler.add_job(lambda: news_update('corazon'), 'interval', hours=5)
-#scheduler.add_job(twitter_update, 'interval', minutes=5)
+scheduler.add_job(twitter_update, 'interval', minutes=5)
+
 
 scheduler.start()
-
