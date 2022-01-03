16fd46c3b9cc7fb06c07a0811e7a1b51a9f66d79
Garazi <ggartzia@gmail.com>
2022-01-03 13:04:36 +0100

    Log when twitter does not return tweets

--- news_puller/shares.py
+++ news_puller/shares.py
@@ -5,12 +5,12 @@ import json
 
 def bearer_oauth(r):
     r.headers["Authorization"] = f"Bearer {cfg.TW_BEARER_TOKEN}"
-    r.headers["User-Agent"] = "v2RecentTweetCountsNews"
+    r.headers["User-Agent"] = 'v2RecentTweetCountsNews'
     return r
 
 
 def callTwitter(search_url, query_params):
-    response = requests.request("GET", search_url, auth=bearer_oauth, params=query_params)
+    response = requests.request('GET', search_url, auth=bearer_oauth, params=query_params)
 
     if response.status_code != 200:
         raise Exception(response.status_code, response.text)
@@ -22,11 +22,11 @@ def shareCount(name):
     count = 0
 
     if name:
-        search_url = "https://api.twitter.com/2/tweets/counts/recent"
+        search_url = 'https://api.twitter.com/2/tweets/counts/recent'
         query_params = {'query': 'url:' + name, 'granularity': 'day'}
 
         response = callTwitter(search_url, query_params)
-        count = response["meta"]["total_tweet_count"]
+        count = response['meta']['total_tweet_count']
 
     return count
 
@@ -34,22 +34,21 @@ def shareCount(name):
 def twitter_shares(new):
     tweet_list= []
     
-    search_url = "https://api.twitter.com/2/tweets/search/recent"
+    search_url = 'https://api.twitter.com/2/tweets/search/recent'
     query_params = {'query': 'url:' + new['name'],
                     'max_results': 100,
                     'tweet.fields': 'id,created_at,author_id, text',
                     'expansions': 'author_id',
                     'user.fields': 'id,name,profile_image_url,username'}
 
-    if "last_tweet" in new:
+    if 'last_tweet' in new:
         query_params['since_id'] = new['last_tweet']
 
-    try:
-      tweets = callTwitter(search_url, query_params)
+    tweets = callTwitter(search_url, query_params)
+    if 'data' in tweets:
       tweet_list = [dict(twt, **{'new':new['_id'], '_id': twt['id']}) for twt in tweets['data']]
       new['last_tweet'] = tweets['meta']['newest_id']
-        
-    except Exception as e:
-      print(e)
+    else:
+      print('Twitter only returned', tweets)
         
     return new, tweet_list
