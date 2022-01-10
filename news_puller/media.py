65e9667f73ec0ed510cafae4c207a7e81bc9899b
Garazi <ggartzia@gmail.com>
2022-01-10 15:03:10 +0100

    Remove logs

--- news_puller/media.py
+++ news_puller/media.py
@@ -1,18 +1,11 @@
 import news_puller.config as cfg
-from logging import getLogger, DEBUG
 from news_puller.db import Database
-
-log = getLogger('werkzeug')
-log.setLevel(DEBUG)
-
 
 def get_media(theme):
     total = []
     media = filter(lambda m: m['theme'] == theme, cfg.PAPER_LIST)
 
     for plist in media:
-        print('Fetch ' + plist['paper'] + ' news from ' + plist['feed'])
-
         plist['numeroNoticias'] = Database.num_news({'paper': plist['paper'], 'theme': theme})
         plist['actualizacion'] = Database.last_new(plist['paper'], theme)
         
