98c37aa9292cd8ab4f0bc5f8cb7de8872a4579f1
Garazi <ggartzia@gmail.com>
2022-02-18 09:21:41 +0100

    Add catch when searching for media

--- news_puller/media.py
+++ news_puller/media.py
@@ -1,16 +1,23 @@
 import news_puller.config as cfg
 from news_puller.db import Database
+from logging import getLogger, DEBUG
+
+logger = getLogger('werkzeug')
+logger.setLevel(DEBUG)
 
 def get_media(theme):
     total = []
     media = filter(lambda m: m['theme'] == theme, cfg.PAPER_LIST)
 
-    for plist in media:
-        plist['numeroNoticias'] = Database.num_news(plist['paper'], theme)
-        print("We have look at numeroNoticias. Value: " + str(plist['numeroNoticias']))
-        plist['actualizacion'] = Database.last_new(plist['paper'], theme)
-        print("We have look at ultima actualizacion. Value: " + str(plist['actualizacion']))
-        
-        total.append(plist)
+    try:
+        for plist in media:
+            plist['numeroNoticias'] = Database.num_news(plist['paper'], theme)
+            plist['actualizacion'] = Database.last_new(plist['paper'], theme)
 
+            total.append(plist)
+
+    except Exception as e:
+        logger.error(e)
+        logger.error('There was an error trying to get the data')
+        
     return total
