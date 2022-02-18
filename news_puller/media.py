e4cbd52faf9c3f85ec2885ee66e9efc5917b44bf
Garazi <ggartzia@gmail.com>
2022-02-18 08:54:03 +0100

    Logs

--- news_puller/media.py
+++ news_puller/media.py
@@ -7,7 +7,9 @@ def get_media(theme):
 
     for plist in media:
         plist['numeroNoticias'] = Database.num_news(plist['paper'], theme)
+        print("We have look at numeroNoticias. Value: " + plist['numeroNoticias'])
         plist['actualizacion'] = Database.last_new(plist['paper'], theme)
+        print("We have look at ultima actualizacion. Value: " + plist['actualizacion'])
         
         total.append(plist)
 
