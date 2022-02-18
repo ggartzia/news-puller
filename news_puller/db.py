1b23dc5224c7aef5226d2db97af43201d37d8b55
Garazi <ggartzia@gmail.com>
2022-02-18 09:49:19 +0100

    Update db.py

--- news_puller/db.py
+++ news_puller/db.py
@@ -142,9 +142,11 @@ class Database(object):
     def last_new(paper, theme):
         mongo_db = Database.DATABASE['news']
         new = mongo_db.find_one({'paper' : paper, 'theme' : theme}, sort=[('published', pymongo.DESCENDING)])
-        print("This is the new: ")
-        print(str(new))
-        return new['published']
+        
+        if new is None:
+            return None
+        else:
+            return new['published']
 
     def num_tweets(new):
         mongo_db = Database.DATABASE['tweets']
