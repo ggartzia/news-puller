37df9f2b1eb72dea1edd925651445f40f38bc8fb
Garazi <ggartzia@gmail.com>
2022-02-28 08:18:17 +0100

    Include error handler

--- news_puller/shares.py
+++ news_puller/shares.py
@@ -12,26 +12,30 @@ api = tweepy.API(auth)
 def tweepy_shares(new):
     tweet_list= []
     users = []
-
-    tweets = tweepy.Cursor(api.search_tweets,
-                           q='url:' + new['fullUrl'],
-                           since_id=new.get('lastTweet', 0) + 1).items(100)
-
-    for tweet in tweets:
-        twt = tweet._json
-
-        users.append({'_id': twt['user']['id'],
-                      'name': twt['user']['name'],
-                      'screen_name': twt['user']['screen_name'],
-                      'image': twt['user']['profile_image_url_https']})
-
-        tweet_list.append({'_id': twt['id'],
-                           'created_at': twt['created_at'],
-                           'text': twt['text'],
-                           'new': new['_id'],
-                           'user': twt['user']['id']})
     
-    Database.save_tweets(tweet_list)
-    Database.save_users(users)
+    try:
+        tweets = tweepy.Cursor(api.search_tweets,
+                               q='url:' + new['fullUrl'],
+                               since_id=new.get('lastTweet', 0) + 1).items(100)
+
+        for tweet in tweets:
+            twt = tweet._json
+
+            users.append({'_id': twt['user']['id'],
+                          'name': twt['user']['name'],
+                          'screen_name': twt['user']['screen_name'],
+                          'image': twt['user']['profile_image_url_https']})
+
+            tweet_list.append({'_id': twt['id'],
+                               'created_at': twt['created_at'],
+                               'text': twt['text'],
+                               'new': new['_id'],
+                               'user': twt['user']['id']})
+
+        Database.save_tweets(tweet_list)
+        Database.save_users(users)
+        
+    except Exception as e:
+        logger.error('Something happened fetching tweets for: %s. %s', new['fullUrl'], e)
 
     return tweet_list
