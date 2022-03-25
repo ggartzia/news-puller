33fea2e29306667c6e72bb850ca8f398ad1b1cfb
Garazi <ggartzia@gmail.com>
2022-03-25 13:38:42 +0100

    Get topics name

--- news_puller/related.py
+++ news_puller/related.py
@@ -2,14 +2,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import euclidean_distances
 
 
+def join_topics(new):
+  return ' '.join([n.get("name") for n in new['topics']])
+
+
 def calculate_similarity(new, data):
   similarity_collection = []
   vectorizer = CountVectorizer()
+  new_topics = join_topics(new)
   
   for compare_to in data:
     corpus = []
-    corpus.append(' '.join(new['topics']))
-    corpus.append(' '.join(compare_to['topics']))
+    corpus.append(new_topics)
+    corpus.append(join_topics(compare_to))
     features = vectorizer.fit_transform(corpus).todense()
 
     distance = euclidean_distances(features[0],features[1])[0][0]
