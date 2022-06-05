766eae1ffed18ef07ec9953dae6e2279ea280325
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-05 14:22:19 +0200

    Removes logs

--- news_puller/scheduler.py
+++ news_puller/scheduler.py
@@ -7,20 +7,15 @@ from apscheduler.triggers.cron import CronTrigger
 
 load_dotenv()
 
-def news_update(paper):
-    api_url = os.getenv('HOST_URL') + 'fetch/' + paper
-    response = requests.get(api_url)
-
 scheduler = BackgroundScheduler()
 
 media = cfg.PAPER_LIST.values()
 minute = round(60 / len(media))
-print('Run every..... ' + str(minute))
 start_at = 0
 
 for plist in media:
-    print('Create scheduler for:::' + str(plist['paper']) + 'in minute:: ' + str(start_at))
-    scheduler.add_job(lambda: news_update(plist['paper']), CronTrigger(minute=start_at, timezone='UTC'))
+    api_url = os.getenv('HOST_URL') + 'fetch/' + plist['paper']
+    scheduler.add_job(lambda: requests.get(api_url), CronTrigger(minute=start_at, timezone='UTC'))
     start_at += minute
 
 scheduler.start()
