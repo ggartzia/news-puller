b6e86e6bea1e320cb1912cc112638fb83118e2cf
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-05 15:21:54 +0200

    Try getting plist as lambda parameter

--- news_puller/scheduler.py
+++ news_puller/scheduler.py
@@ -7,20 +7,20 @@ from news_puller.db import Database
 
 load_dotenv()
 
+
+def news_update(paper):
+    api_url = os.getenv('HOST_URL') + 'fetch/' + paper
+    response = requests.get(api_url)
+
+
 scheduler = BackgroundScheduler()
 
 media = Database.select_all_media()
 minute = round(60 / len(media))
 start_at = 0
 
-
-#job_list = []
 for plist in media:
-    api_url = os.getenv('HOST_URL') + 'fetch/' + plist['paper']
-    #job_list.append(lambda job=job: scheduler.add_job(lambda: requests.get(api_url), CronTrigger(minute=start_at, timezone='UTC')))
-    scheduler.add_job(lambda job=job: requests.get(api_url), CronTrigger(minute=start_at, timezone='UTC'))
+    scheduler.add_job(lambda plist=plist: news_update(plist['paper']), CronTrigger(minute=start_at, timezone='UTC'))
     start_at += minute
-
-#for j in job_list: j()
 
 scheduler.start()
