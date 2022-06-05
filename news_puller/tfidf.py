7d5f702626b72ad348399ef9a1264fd52e8b7bf1
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-05 23:11:41 +0200

    Use TokTokTockenizer

--- news_puller/tfidf.py
+++ news_puller/tfidf.py
@@ -1,5 +1,6 @@
 from logging import getLogger, DEBUG
 from nltk import sent_tokenize, word_tokenize
+from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SpanishStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -15,10 +16,10 @@ STEMMER = SpanishStemmer()
 
 def tokenize_and_stem(text):
     print('OMG Stemmer')
-    tokens = [word.lower() for sent in sent_tokenize(text) for word in word_tokenize(sent)]
+    toktok = ToktokTokenizer()
     filtered_tokens = []
     # filter out any tokens not containing letters (e.g., numeric tokens, raw punctuation)
-    for token in tokens:
+    for token in toktok.tokenize(sent):
         if re.search('[a-zA-Z]', token):
             filtered_tokens.append(token)
     stems = [STEMMER.stem(t) for t in filtered_tokens]
@@ -29,19 +30,19 @@ def tokenize_and_stem(text):
 def get_topics(corpus):
     words = []
     try:
-        print('Garaziiiiiii count words 1')
+        print('Garaziiiiiii tfidf 1')
         vec = TfidfVectorizer(stop_words=STOP_WORDS,
                               use_idf=True, 
                               tokenizer=tokenize_and_stem,
                               ngram_range=(1,3)).fit(corpus)
-        print('Garaziiiiiii count words 2')
+        print('Garaziiiiiii tfidf 2')
         bag_of_words = vec.transform(corpus)
-        print('Garaziiiiiii count words %s', bag_of_words)
+        print('Garaziiiiiii tfidf %s', bag_of_words)
         sum_words = bag_of_words.sum(axis=0)
-        print('Garaziiiiiii count words %s', sum_words)
+        print('Garaziiiiiii tfidf %s', sum_words)
         words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
         words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
-        print('Garaziiiiiii count words %s', words_freq)
+        print('Garaziiiiiii tfidf %s', words_freq)
         words = words_freq[:10]
     except Exception as e:
         logger.error('Failed counting words in article. Error: %s', e)
