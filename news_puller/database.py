571e6981cea6946477009c04052cb671e90f9425
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-05 17:02:01 +0200

    Get URI

--- news_puller/database.py
+++ news_puller/database.py
@@ -7,7 +7,7 @@ load_dotenv()
 class Database(object):
     
     URI = 'mongodb+srv://%s:%s@newscluster.3saws.mongodb.net/news?retryWrites=true&w=majority' % (os.getenv('MONGO_USERNAME'), os.getenv('MONGO_PASSWORD'))
-    client = pymongo.MongoClient(Database.URI)  # establish connection with database
+    client = pymongo.MongoClient(URI)  # establish connection with database
     DATABASE = client['news']
-    
+
     PAGE_SIZE = 12
