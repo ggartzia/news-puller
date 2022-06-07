53941fb1b0b1da73890e20f226816835af771fce
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-07 20:54:56 +0200

    convert string to json

--- news_puller/comments.py
+++ news_puller/comments.py
@@ -3,6 +3,7 @@ import tweepy
 import json
 from dotenv import load_dotenv
 from logging import getLogger, DEBUG
+from news_puller.db.user import save_user
 from news_puller.db.comment import save_comment
 
 
@@ -18,12 +19,31 @@ follow = ["121183700", "14436030", "74453123"]
 class FetchStatus(tweepy.Stream):
 
     def on_data(self, data):
-        tweet = data.decode('utf8').replace("'", '"')
-        comment = {key: tweet[key] for key in ["created_at", "id", "text", "user", "retweeted_status", "user_mentions"]}
-        #The media?
-        #The new?
-        #Save user?
-        save_comment(comment)
+        try:
+            print(data)
+            
+            tweet = json.loads(data.decode('utf8'))
+            print(tweet)
+            print(data._json)
+            comment = {"_id": tweet["id_str"],
+                       "created_at": tweet["created_at"],
+                       "text": tweet["text"],
+                       "user": tweet['user']['id'],
+                       "retweeted_status": tweet["retweeted_status"], 
+                       "user_mentions": tweet["user_mentions"]}
+
+            print(comment)
+            save_user({'id': tweet['user']['id'],
+                       'name': tweet['user']['name'],
+                       'screen_name': tweet['user']['screen_name'],
+                       'image': tweet['user']['profile_image_url_https']})
+
+            #The media?
+            #The new?
+            save_comment(comment)
+
+        except Exception as e:
+            logger.error('Something happened fetching tweets: %s', e)
 
     def on_error(self, status):
         print(status)
