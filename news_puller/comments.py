7d030d120671adb43bbf3ad2e8d606761040253a
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-07 17:43:26 +0200

    Fix Stream class

--- news_puller/comments.py
+++ news_puller/comments.py
@@ -1,8 +1,6 @@
 import json
 import os
 import tweepy
-from tweepy import Stream
-from tweepy.streaming import StreamListener
 from dotenv import load_dotenv
 from logging import getLogger, DEBUG
 
@@ -13,22 +11,37 @@ logger = getLogger('werkzeug')
 logger.setLevel(DEBUG)
 
 
-class StdOutListener(StreamListener):
-    def on_data(self, data):
-        clean_data = json.loads(data)
-        print("This is a tweet: %s", clean_data)
+auth = tweepy.OAuthHandler(os.getenv('TW_CONSUMER_KEY'), os.getenv('TW_CONSUMER_SECRET'))
+auth.set_access_token(os.getenv('TW_ACCESS_TOKEN'), os.getenv('TW_ACCESS_TOKEN_SECRET'))
 
-    def setUpAuth():
-        stream = tweepy.Stream(
-            os.getenv('TW_CONSUMER_KEY'), os.getenv('TW_CONSUMER_SECRET'),
-            os.getenv('TW_ACCESS_TOKEN'), os.getenv('TW_ACCESS_TOKEN_SECRET'))
+api = tweepy.API(auth)
 
-        return stream
+# Customize tweepy.SteamListener
+class MyStreamListener(tweepy.StreamListener):
+    """
+    Twitter listener, collects streaming tweets and output to a file
+    """
+    def __init__(self, api=None):
+        super(MyStreamListener, self).__init__()
+        self.num_tweets = 0
 
-    def followStream():
-        print("This is the follow stream")
-        stream.filter(follow=["121183700"], is_async=True)
+    def on_status(self, status):
+        tweet = status._json
+        print("Garaziii tweet: %s", tweet)
+        self.num_tweets += 1
+        
 
+    def on_error(self, status):
+        print(status)
+        logger.error('Something happened fetching tweets: %s', status)
 
-if __name__ == "__main__":
-    followStream()
+media = select_all_media()
+for i in range(media):
+    # Initialize Stream listener
+    l = MyStreamListener(i)
+
+    # Create you Stream object with authentication
+    stream = tweepy.Stream(auth, l)
+    print("This is a paper:: %s", media[i])
+    # Filter Twitter Streams to capture data by the keywords:
+    stream.filter(follow=media[i].twitter_id, is_async=True)
