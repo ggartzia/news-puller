cc353986ff3e50b6ee13909dfa1d3bd1baa3edbe
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-07 17:56:02 +0200

    Replace StreamListener with Stream

--- news_puller/comments.py
+++ news_puller/comments.py
@@ -11,37 +11,40 @@ logger = getLogger('werkzeug')
 logger.setLevel(DEBUG)
 
 
-auth = tweepy.OAuthHandler(os.getenv('TW_CONSUMER_KEY'), os.getenv('TW_CONSUMER_SECRET'))
-auth.set_access_token(os.getenv('TW_ACCESS_TOKEN'), os.getenv('TW_ACCESS_TOKEN_SECRET'))
-
-api = tweepy.API(auth)
-
 # Customize tweepy.SteamListener
-class MyStreamListener(tweepy.StreamListener):
+class MyStreamListener(tweepy.Stream):
     """
     Twitter listener, collects streaming tweets and output to a file
     """
     def __init__(self, api=None):
         super(MyStreamListener, self).__init__()
-        self.num_tweets = 0
+
 
     def on_status(self, status):
         tweet = status._json
-        print("Garaziii tweet: %s", tweet)
-        self.num_tweets += 1
+        print("Garaziii status: %s", tweet)
         
+
+    def on_data(self, data):
+        tweet = data._json
+        print("Garaziii data: %s", tweet)
+
+
 
     def on_error(self, status):
         print(status)
         logger.error('Something happened fetching tweets: %s', status)
 
-media = select_all_media()
-for i in range(media):
-    # Initialize Stream listener
-    l = MyStreamListener(i)
 
-    # Create you Stream object with authentication
-    stream = tweepy.Stream(auth, l)
-    print("This is a paper:: %s", media[i])
-    # Filter Twitter Streams to capture data by the keywords:
-    stream.filter(follow=media[i].twitter_id, is_async=True)
+media = select_all_media()
+
+follow = ["121183700", "14436030", "74453123"]
+
+# Create you Stream object with authentication
+auth = tweepy.OAuthHandler(os.getenv('TW_CONSUMER_KEY'), os.getenv('TW_CONSUMER_SECRET'))
+auth.set_access_token(os.getenv('TW_ACCESS_TOKEN'), os.getenv('TW_ACCESS_TOKEN_SECRET'))
+# Initialize Stream listener
+l = MyStreamListener(i)
+
+stream = tweepy.Stream(auth, l)
+stream.filter(follow=follow, is_async=True)
