71688c68724eedafa1f57da50830792166480e8c
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-12 09:33:24 +0200

    Do not lemmatize all words

--- news_puller/tfidf.py
+++ news_puller/tfidf.py
@@ -1,10 +1,10 @@
+import spacy
 from logging import getLogger, DEBUG
 from news_puller.utils import clean_html
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report, accuracy_score
 from sentiment_analysis_spanish import sentiment_analysis
 from nltk.corpus import stopwords
-import spacy
 
 logger = getLogger('werkzeug')
 logger.setLevel(DEBUG)
@@ -15,16 +15,17 @@ class TfIdfAnalizer(object):
     def __init__(self):
         try:
             self.STOP_WORDS = set(stopwords.words('spanish'))
-            self.NLP = spacy.load("en_core_web_sm")
+            self.NLP = spacy.load('en_core_web_sm')
         except: # If not present, we download
-            spacy.cli.download("en_core_web_sm")
-            self.NLP = spacy.load("en_core_web_sm")
+            spacy.cli.download('en_core_web_sm')
+            self.NLP = spacy.load('en_core_web_sm')
 
 
     def get_topics(self, corpus, size=6):
         words = []
         try:
             vec = TfidfVectorizer(stop_words=self.STOP_WORDS,
+                                  analyzer='word',
                                   tokenizer=self.tokenize_stem,
                                   ngram_range=(1,2)).fit(corpus)
             bag_of_words = vec.transform(corpus)
@@ -33,6 +34,7 @@ class TfIdfAnalizer(object):
             words_freq = sorted(words_freq, key = lambda x: x[1], reverse=True)
             words = [w[0] for w in words_freq[:size]]
 
+            print(words)
         except Exception as e:
             logger.error('Failed counting words in article. Error: %s', e)
 
@@ -43,7 +45,9 @@ class TfIdfAnalizer(object):
         tokens = []
         doc = self.NLP(text)
         for token in doc:
-            if re.search('[a-zA-Z]', token.text):
+            if toke.pos_ in ('NOUN', 'PROPN'):
+                tokens.append(token.text)
+            elif token.pos_ in ('ADJ', 'ADV', 'VERB'):
                 tokens.append(token.lemma_)
         return tokens
 
@@ -52,22 +56,23 @@ class TfIdfAnalizer(object):
         rate = 0
         try:
             sentiment = sentiment_analysis.SentimentAnalysisSpanish()
-            rate = sentiment.sentiment(text)
-            print(rate)
+            rate = 10 * sentiment.sentiment(text)
+
+            if rate < 4:
+                return 'negative'
+            elif rate < 7:
+                return 'neutral'
+            else:
+                return 'positive'
+
         except Exception as e:
             logger.error('Failed analysing sentiment of tweet. Error: %s', e)
 
-        return rate * 10
-
 
     def rate_feeling(self, text):
-        rate = 0
-
         try:
             text = clean_html(text)
-            rate = self.count_polarity_words(text)
+            return self.count_polarity_words(text)
             
         except Exception as e:
             logger.error('There was an error analysing the text of the tweet: %s', e)
-
-        return rate
