3e26a5cc3ad6e4da775e3daf2ced8f055effdc1e
Gartzia Gartzia <g-gartzia@lanbide.eus>
2022-06-15 07:00:20 +0200

    remove logs

--- news_puller/scrapper.py
+++ news_puller/scrapper.py
@@ -20,6 +20,11 @@ class NewsScrapper(object):
 
     def scrap(self, url, paper):
         new_id = utils.create_unique_id(url)
+        title = ''
+        desc = ''
+        date = ''
+        topics = []
+        image = ''
 
         if search_new(new_id) is None:
 
@@ -28,32 +33,80 @@ class NewsScrapper(object):
                 if (page.status_code == 200):
                     soup = BeautifulSoup(page.text, 'html.parser')
 
+                    title = self.get_title(soup)
+                    desc = self.get_description(soup)
+                    date = self.get_date(soup)
+                    image =  self.get_image(soup)
                     article = soup.find("article")
                     text = [e.get_text() for e in article.find_all('p')]
-                    print("text ----->>>>> %s", text)
                     topics = self.TFIDF.get_topics(text)
 
                     media = search_media(paper)
 
                     new = {'id': new_id,
                            'fullUrl': url,
-                           'title': soup.find("meta", property="og:title")['content'],
-                           'description': soup.find("meta", property="og:description")['content'],
+                           'title': self.get_title(soup),
+                           'description': self.get_description(soup),
                            'paper': media['_id'],
                            'theme': media['theme'],
-                           'published': utils.parse_date(soup.find("meta", property="article:published_time")['content']),
+                           'published': self.get_date(soup),
                            'topics': topics,
-                           'image': soup.find("meta", property="twitter:image")['content']
+                           'image': self.get_image(soup)
                           }
                     
                     save_topics(topics, media['theme'])
                     save_new(new)
 
                     return new_id
+        
             except Exception as e:
-                logger.error('There was an error parsing new url: %s. %s', url, e)
+                logger.error('There was an error parsing new url: %s, %s, %s, %s, %s, %s. %s', url, title, desc, date, image, topics, e)
 
         else:
             return new_id
 
         return None
+
+
+    def get_description(self, body):
+        description = ''
+
+        tag = soup.find("meta", property="og:description")
+        if tag is not None:
+            description = tag['content']
+        
+        # Remove html tags from description
+        return description
+
+
+    def get_title(self, body):
+        title = ''
+
+        tag = soup.find("meta", property="og:title")
+        if tag is not None:
+            title = tag['content']
+        
+        # Remove html tags from description
+        return title
+
+
+    def get_date(self, body):
+        date = ''
+
+        tag = soup.find("meta", property="article:published_time")
+        if tag is not None:
+            date = tag['content']
+        
+        # Remove html tags from description
+        return utils.parse_date(date)
+
+
+    def get_image(self, body):
+        image = ''
+
+        tag = soup.find("meta", property="twitter:image")
+        if tag is not None:
+            image = tag['content']
+        
+        # Remove html tags from description
+        return image
